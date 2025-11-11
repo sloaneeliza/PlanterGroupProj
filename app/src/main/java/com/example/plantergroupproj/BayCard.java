2db51e3845655b7ddb6adea2e6cfc415cbbf18a4
Elle Simonds <ellesimonds@gmail.com>
2025-11-11 12:01:42 -0600

    added rest of card layouts and info under card classes. ES.

--- app/src/main/java/com/example/plantergroupproj/BayCard.java
+++ app/src/main/java/com/example/plantergroupproj/BayCard.java
@@ -1,4 +1,18 @@
 package com.example.plantergroupproj;
 
-public class BayCard {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BayCard extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bay_card);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.recipeFragment, RecipeFragment.newInstance("Bay"))
+                .commit();
+    }
 }
+
