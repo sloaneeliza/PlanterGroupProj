5a11fc08babb39bfb11ee89902f8c9fd5653a84c
Elle Simonds <ellesimonds@gmail.com>
2025-11-20 11:42:55 -0600

    Fixed MintCard.java and activity_mint_card.xml

--- app/src/main/java/com/example/plantergroupproj/MintCard.java
+++ app/src/main/java/com/example/plantergroupproj/MintCard.java
@@ -1,6 +1,10 @@
 package com.example.plantergroupproj;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,9 +13,39 @@ public class MintCard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mint_card);
-
+        ImageButton backButton = findViewById(R.id.backBtnMint);
+        backButton.setOnClickListener(v -> {
+            finish();
+        });
+        RecipeFragment fragment = RecipeFragment.newInstance("Mint");
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.recipeContainer, RecipeFragment.newInstance("Mint"))
+                .replace(R.id.recipeContainer, fragment)
                 .commit();
+
+        TextView homeBtn = findViewById(R.id.homeBtnMint);
+
+        homeBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MintCard.this, HomePageActivity.class);
+            startActivity(intent);
+        });
+        TextView saveHerbBtn = findViewById(R.id.saveMintBtn);
+
+        saveHerbBtn.setOnClickListener(v -> {
+            saveHerb();
+        });
+
+    }
+
+    @SuppressLint("MutatingSharedPrefs")
+    private void saveHerb() {
+        android.content.SharedPreferences prefs = getSharedPreferences("MyHerbs", MODE_PRIVATE);
+        android.content.SharedPreferences.Editor editor = prefs.edit();
+
+        java.util.Set<String> savedHerbs = prefs.getStringSet("savedHerbs", new java.util.HashSet<>());
+        savedHerbs.add("Mint");
+
+        editor.putStringSet("savedHerbs", savedHerbs);
+        editor.apply();
+        android.widget.Toast.makeText(this, "Mint" + " saved!", android.widget.Toast.LENGTH_SHORT).show();
     }
 }
