7e32710497ed171caac8d50f70cffc842afba03e
Elle Simonds <ellesimonds@gmail.com>
2025-11-20 11:16:03 -0600

    added java code for ParsleyCard.java

--- app/src/main/java/com/example/plantergroupproj/ParsleyCard.java
+++ app/src/main/java/com/example/plantergroupproj/ParsleyCard.java
@@ -1,6 +1,10 @@
 package com.example.plantergroupproj;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,9 +13,38 @@ public class ParsleyCard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parsley_card);
-
+        ImageButton backButton = findViewById(R.id.backBtnParsley);
+        backButton.setOnClickListener(v -> {
+            finish();
+        });
+        RecipeFragment fragment = RecipeFragment.newInstance("Parsley");
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.recipeContainer, RecipeFragment.newInstance("Parsley"))
+                .replace(R.id.recipeContainer, fragment)
                 .commit();
+
+        TextView homeBtn = findViewById(R.id.homeBtnParsley);
+
+        homeBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(ParsleyCard.this, HomePageActivity.class);
+            startActivity(intent);
+        });
+        TextView saveHerbBtn = findViewById(R.id.saveParsleyBtn);
+
+        saveHerbBtn.setOnClickListener(v -> {
+            saveHerb();
+        });
+
+    }
+    @SuppressLint("MutatingSharedPrefs")
+    private void saveHerb() {
+        android.content.SharedPreferences prefs = getSharedPreferences("MyHerbs", MODE_PRIVATE);
+        android.content.SharedPreferences.Editor editor = prefs.edit();
+
+        java.util.Set<String> savedHerbs = prefs.getStringSet("savedHerbs", new java.util.HashSet<>());
+        savedHerbs.add("Parsley");
+
+        editor.putStringSet("savedHerbs", savedHerbs);
+        editor.apply();
+        android.widget.Toast.makeText(this, "Parsley" + " saved!", android.widget.Toast.LENGTH_SHORT).show();
     }
 }
