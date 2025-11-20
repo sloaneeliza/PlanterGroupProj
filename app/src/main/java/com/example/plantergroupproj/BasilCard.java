49ca21ae8dad3783c39bdb46552a4c77055a762a
Elle Simonds <ellesimonds@gmail.com>
2025-11-20 11:24:11 -0600

    fixed errors in BasilCard.java

--- app/src/main/java/com/example/plantergroupproj/BasilCard.java
+++ app/src/main/java/com/example/plantergroupproj/BasilCard.java
@@ -1,5 +1,6 @@
 package com.example.plantergroupproj;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
@@ -31,21 +32,22 @@ public class BasilCard extends AppCompatActivity {
         TextView saveHerbBtn = findViewById(R.id.saveBasilBtn);
 
         saveHerbBtn.setOnClickListener(v -> {
-            saveHerb("Basil");
+            saveHerb();
         });
 
     }
 
-    private void saveHerb(String herbName) {
+    @SuppressLint("MutatingSharedPrefs")
+    private void saveHerb() {
         android.content.SharedPreferences prefs = getSharedPreferences("MyHerbs", MODE_PRIVATE);
         android.content.SharedPreferences.Editor editor = prefs.edit();
 
         java.util.Set<String> savedHerbs = prefs.getStringSet("savedHerbs", new java.util.HashSet<>());
-        savedHerbs.add(herbName);
+        savedHerbs.add("Basil");
 
         editor.putStringSet("savedHerbs", savedHerbs);
         editor.apply();
-        android.widget.Toast.makeText(this, herbName + " saved!", android.widget.Toast.LENGTH_SHORT).show();
+        android.widget.Toast.makeText(this, "Basil" + " saved!", android.widget.Toast.LENGTH_SHORT).show();
     }
 
 }
