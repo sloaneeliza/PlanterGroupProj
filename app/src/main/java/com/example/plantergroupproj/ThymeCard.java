e3e4f1d8854f239bb70e04a8306c40f41f4faf30
Elle Simonds <ellesimonds@gmail.com>
2025-11-20 12:08:47 -0600

    Fixed ThymeCard.java and activity_thyme_card.xml

--- app/src/main/java/com/example/plantergroupproj/ThymeCard.java
+++ app/src/main/java/com/example/plantergroupproj/ThymeCard.java
@@ -1,6 +1,10 @@
 package com.example.plantergroupproj;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,9 +13,41 @@ public class ThymeCard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thyme_card);
-
+        ImageButton backButton = findViewById(R.id.backBtnThyme);
+        backButton.setOnClickListener(v -> {
+            finish();
+        });
+        RecipeFragment fragment = RecipeFragment.newInstance("Thyme");
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.recipeContainer, RecipeFragment.newInstance("Thyme"))
+                .replace(R.id.recipeContainer, fragment)
                 .commit();
+
+        TextView homeBtn = findViewById(R.id.homeBtnThyme);
+
+        homeBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(ThymeCard.this, HomePageActivity.class);
+            startActivity(intent);
+        });
+        TextView saveHerbBtn = findViewById(R.id.saveThymeBtn);
+
+        saveHerbBtn.setOnClickListener(v -> {
+            saveHerb();
+        });
+
+    }
+
+
+
+    @SuppressLint("MutatingSharedPrefs")
+    private void saveHerb() {
+        android.content.SharedPreferences prefs = getSharedPreferences("MyHerbs", MODE_PRIVATE);
+        android.content.SharedPreferences.Editor editor = prefs.edit();
+
+        java.util.Set<String> savedHerbs = prefs.getStringSet("savedHerbs", new java.util.HashSet<>());
+        savedHerbs.add("Thyme");
+
+        editor.putStringSet("savedHerbs", savedHerbs);
+        editor.apply();
+        android.widget.Toast.makeText(this, "Thyme" + " saved!", android.widget.Toast.LENGTH_SHORT).show();
     }
 }
