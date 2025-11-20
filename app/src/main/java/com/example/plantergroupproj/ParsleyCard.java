9d6089edc89b7e1e5303e56880e9898bbe4e80a5
Elle Simonds <ellesimonds@gmail.com>
2025-11-20 11:22:20 -0600

    fixed ParsleyCard.java

--- app/src/main/java/com/example/plantergroupproj/ParsleyCard.java
+++ app/src/main/java/com/example/plantergroupproj/ParsleyCard.java
@@ -1,5 +1,6 @@
 package com.example.plantergroupproj;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
@@ -31,21 +32,22 @@ public class ParsleyCard extends AppCompatActivity {
         TextView saveHerbBtn = findViewById(R.id.saveParsleyBtn);
 
         saveHerbBtn.setOnClickListener(v -> {
-            saveHerb("Parsley");
+            saveHerb();
         });
 
     }
 
-    private void saveHerb(String herbName) {
+    @SuppressLint("MutatingSharedPrefs")
+    private void saveHerb() {
         android.content.SharedPreferences prefs = getSharedPreferences("MyHerbs", MODE_PRIVATE);
         android.content.SharedPreferences.Editor editor = prefs.edit();
 
         java.util.Set<String> savedHerbs = prefs.getStringSet("savedHerbs", new java.util.HashSet<>());
-        savedHerbs.add(herbName);
+        savedHerbs.add("Parsley");
 
         editor.putStringSet("savedHerbs", savedHerbs);
         editor.apply();
-        android.widget.Toast.makeText(this, herbName + " saved!", android.widget.Toast.LENGTH_SHORT).show();
+        android.widget.Toast.makeText(this, "Parsley" + " saved!", android.widget.Toast.LENGTH_SHORT).show();
     }
 
 }
